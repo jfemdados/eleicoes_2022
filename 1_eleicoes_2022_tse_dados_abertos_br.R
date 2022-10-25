695ce8aa326884f109feaea172f5b6045b12eb10
Marcello Filgueiras <53457944+marcellofilgueiras@users.noreply.github.com>
2022-10-25 15:09:45 -0300

    dados presidenciais em jf #3

--- 1_eleicoes_2022_tse_dados_abertos_br.R
+++ 1_eleicoes_2022_tse_dados_abertos_br.R
@@ -10,12 +10,17 @@ library(tidyverse)
 
 # Importing ---------------------------------------------------------------
 
+# Esse DF está disponiível para download no site oficial do TSE
+# Escolha "BR - Votação por seção eleitoral - 2022" dentro de https://dadosabertos.tse.jus.br/dataset/resultados-2022"
+
 votacao_secao_2022_br_raw <- readr::read_delim("tse_dados_abertos/data_raw/votacao_secao_2022_BR.csv", 
                                                    delim = ";",
                                                    escape_double = FALSE,
                                                    trim_ws = TRUE,
                                                    locale = locale(encoding = "latin1")
-)
+                                               #,col_select = !c("DT_GERACAO","HH_GERACAO","TP_ABRANGENCIA",
+                                                #                   )
+                                                )
 
 votacao_secao_2022_jf_presid<- votacao_secao_2022_br_raw %>%
   janitor::clean_names() %>%
@@ -26,7 +31,7 @@ votacao_secao_2022_jf_presid<- votacao_secao_2022_br_raw %>%
 
 # Checando a Base ---------------------------------------------------------
 
-#Números pra ver se a base está correta 
+#Cúmeros pra ver se a base está correta 
 
 # Comparando Bases
 
@@ -41,16 +46,16 @@ votacao_secao_2022_jf_presid %>%
 
 
 votacao_secao_2022_br_raw %>%
-  count(DS_CARGO)
+  count(TP_ABRANGENCIA)
 
 
 #Counts
 
 votacao_secao_2022_jf_presid %>%
   filter(ds_cargo == "PRESIDENTE") %>% 
-  dplyr::group_by(nm_votavel) %>%
-  dplyr::tally(qt_votos, sort = TRUE) %>%
-  mutate(votos_prop= proportions(n))
+    dplyr::group_by(nm_votavel) %>%
+    dplyr::tally(qt_votos, sort = TRUE) %>%
+    mutate(votos_prop= proportions(n))
 
 # Resultados bateram com votos totais
 #(percentual votos ao total, não votos válidos )
@@ -100,7 +105,10 @@ votacao_secao_2022_jf_presid_wide <- votacao_secao_2022_jf_presid %>%
 
 
 
-#Checando--------
+
+# Checando se a base está boa ----------------------------------------------------------------
+
+
 
 #Não temos urnas sem valores...
 is.na(votacao_secao_2022_jf_presid_wide $ jair_messias_bolsonaro)%>% 
@@ -112,7 +120,7 @@ votacao_secao_2022_jf_presid_wide %>%
           jair_messias_bolsonaro, luiz_inacio_lula_da_silva) #%>% view()
 
 
-#Mass temos um problema --------
+#Mass Mass temos um problema 
 
 #Mas temos 5 seções que tem mesmo local de votação
 #podem estar em zonas diferentes mas mesmo lugar
@@ -131,3 +139,11 @@ votacao_secao_2022_jf_presid_wide %>%
     count( nr_zona, nr_secao,
            nr_local_votacao,
            sort= TRUE) 
+
+
+
+# Exporting ---------------------------------------------------------------
+
+
+  
+  
