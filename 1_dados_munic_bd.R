e1163055725af24fce657054ed9a3f3a6896dd00
Marcello Filgueiras <53457944+marcellofilgueiras@users.noreply.github.com>
2022-10-26 19:48:35 -0300

    Bases pela BD e Gráficos #2

--- 1_dados_munic_bd.R
+++ 1_dados_munic_bd.R
@@ -1,6 +1,5 @@
 #Eleicoes 2022 - Mapa por bairro JF
-#Autores:  Marcello (Count e Faxina de Dados TSE)
-#           Arthur (precisao da Geolocalização das Urnas)
+#Autores:  Marcello e Arthur
 
 library(tidyverse)
 library(basedosdados)
@@ -13,15 +12,15 @@ library(basedosdados)
 basedosdados::set_billing_id("eleicoes2022jf")
 
 
-### Resultados Governadores e Presidente por Município 
+### Resultados Governadores e Presidente por MunicÃ­pio 
 
-mg_resultados_exec_query <- basedosdados::bdplyr("br_tse_eleicoes.resultados_candidato_municipio") %>%
+mg_result_exec_query <- basedosdados::bdplyr("br_tse_eleicoes.resultados_candidato_municipio") %>%
                     dplyr::filter(ano == 2022 & sigla_uf == "MG"
                                   & str_detect(cargo,"presidente|governador")
                                   )
               
               
-mg_resultados_exec_data <- basedosdados::bd_collect(mg_resultados_exec_query)
+mg_result_exec_data <- basedosdados::bd_collect(mg_result_exec_query)
 
 
 ### Detalhes dos Candidatos - Nomes
@@ -36,16 +35,18 @@ mg_candidatos_data <- basedosdados::bd_collect(mg_candidatos_query)
 
 
 
-#Join com Nomes =D
 
-mg_result_nome_join<- inner_join(mg_resultados_exec_data,
+# PÃ³s BD- Joining e Tidying -----------------------------------------------
+
+
+mg_result_nome_join<- inner_join(mg_result_exec_data,
                                 mg_candidatos_data 
                                 #, by = "id_candidato_bd" #tirei pq peguei colunas a mais
                                 # e assim o Rn deixar o R juntar colunas iguais
-                                # n houve prejuízos com inner_join
+                                # n houve prejuÃ­zos com inner_join
                                 )
 
-# Número de Município bate
+# NÃºmero de MunicÃ­pio bate
 mg_result_nome_join %>% 
   filter(nome_urna == "Jair Bolsonaro") %>%
     count(nome_urna)
@@ -56,34 +57,68 @@ mg_result_nome_join %>%
   group_by(id_municipio, cargo, nome_urna) %>%
     summarize(max(votos))
 
+# ERRO: pq quando vocÃª agrupa por tudo ele summariza nada kkk
 
 #Colocando pra Wide
 
+# Tive Que fazer dois DFs pq o join estava dando erro
+
+#Presidente
+
 mg_result_pres_wide<- mg_result_nome_join %>%
+  #Sem Filtrar o Pivot Buga
   filter(cargo== "presidente") %>%
+  #tive que tirar todos as colunas que remetiam Ã  identificaÃ§Ã£o do candidato
+  #pq o pivot wider estava dando erro
   select(!c(numero_partido, sigla_partido,  numero_partido,
             numero_candidato, sequencial_candidato, id_candidato_bd,
             sequencial, resultado)
          ) %>%
+  # Pivotando para juntar com o mapa do {geobr}
   tidyr::pivot_wider(names_from = nome_urna,
               values_from = votos) %>%
   janitor::clean_names() %>%
-  mutate(mais_votado= if_else(lula >= jair_bolsonaro,
+  # Jeito Feio de Fazer conta kkk
+  mutate(mais_votado= if_else(lula > jair_bolsonaro,
                               true = "Lula",
                               false = "Bolsonaro")) %>%
   dplyr::relocate(.after = cargo, mais_votado, lula, jair_bolsonaro)
 
+#Governador
 
 mg_result_gov_wide<- mg_result_nome_join %>%
   filter(cargo== "governador") %>%
+  #tive que tirar todos as colunas que remetiam Ã  identificaÃ§Ã£o do candidato
+  #pq o pivot wider estava dando erro
   select(!c(numero_partido, sigla_partido,  numero_partido,
             numero_candidato, sequencial_candidato, id_candidato_bd,
             sequencial, resultado)
   ) %>%
+  # Pivotando para juntar com o mapa do {geobr}
   pivot_wider(names_from = nome_urna,
               values_from = votos)  %>%
   janitor::clean_names() %>%
-  mutate(mais_votado= if_else(zema >= kalil,
+  # Jeito Feio de Fazer conta kkk
+  mutate(mais_votado= if_else(zema > kalil,
+                              true = "Zema",
+                              false = "Kalil")) %>%
+  dplyr::relocate(.after = cargo, mais_votado, zema, kalil)
+
+
+mg_result_exec_wide<- mg_result_nome_join %>%
+  filter(cargo== "governador") %>%
+  #tive que tirar todos as colunas que remetiam Ã  identificaÃ§Ã£o do candidato
+  #pq o pivot wider estava dando erro
+  select(!c(numero_partido, sigla_partido,  numero_partido,
+            numero_candidato, sequencial_candidato, id_candidato_bd,
+            sequencial, resultado)
+  ) %>%
+  # Pivotando para juntar com o mapa do {geobr}
+  pivot_wider(names_from = nome_urna,
+              values_from = votos)  %>%
+  janitor::clean_names()# %>%
+  # Jeito Feio de Fazer conta kkk
+  mutate(mais_votado= if_else(zema > kalil,
                               true = "Zema",
                               false = "Kalil")) %>%
   dplyr::relocate(.after = cargo, mais_votado, zema, kalil)
@@ -91,11 +126,9 @@ mg_result_gov_wide<- mg_result_nome_join %>%
 
 
 
+# VisualizaÃ§Ã£o com Mapas =D -----------------------------------------------
 
-
-# Visualização com Mapas =D -----------------------------------------------
-
-# Será que vai?
+# SerÃ¡ que vai?
 
 
 library(geobr)
@@ -107,10 +140,20 @@ tmap_mode("view")
 
 mapa_munic_mg <- geobr::read_municipality(code_muni = "MG")
 
+base_ibge_raw <- read_rds("mapas/base_ibge.rds")
+
+
+# Juntando Mapas
+
+base_ibge <- base_ibge_raw %>%
+  select(Cod_setor, Cod_meso:Nome_do_bairro) %>%
+  filter(Nome_do_municipio == "JUIZ DE FORA") %>%
+  rename(code_tract = Cod_setor)
+
 # Presidente mais votado
 
-select(mg_result_pres_wide, mais_votado, lula, jair_bolsonaro,
-       id_municipio)
+
+# Join Shape File e Dados
 
 mapa_vot_presid <-  full_join( x = mapa_munic_mg %>%
                                 mutate(id_municipio= as.character(code_muni)),
@@ -122,23 +165,23 @@ mapa_vot_presid <-  full_join( x = mapa_munic_mg %>%
 
 
 
-### ggplot
+### Plotando o Mapa
 mapa_vot_presid %>%
 ggplot() +
-  geom_sf( aes( fill= mais_votado) ,size= .15,
-           color= "grey25", show.legend = TRUE) +
+  geom_sf( aes( fill= mais_votado) ,size= .20,
+           color= "grey20", show.legend = TRUE) +
         theme_void() +
   scale_fill_manual( name= "Candidato",
                      #label= c("Menos de 20%", "20% a 40%"),
                      values= c("dodgerblue3","firebrick1")) +
-  labs(title = "Eleições 2022: Presidente mais Votados por Município em MG", 
-       caption = "DEU CERTO FILHA DA PUTA")
+  labs(title = "EleiÃ§Ãµes 2022: Presidente mais Votados por MunicÃ­pio em MG", 
+       #caption = "Fonte"
+       )
   
 
 # Governador mais votado
 
-select(mg_result_gov_wide, mais_votado, zema, kalil,
-       id_municipio)
+
 
 mapa_vot_gov <-  full_join( x = mapa_munic_mg %>%
                                  mutate(id_municipio= as.character(code_muni)),
@@ -153,13 +196,55 @@ mapa_vot_gov <-  full_join( x = mapa_munic_mg %>%
 ### ggplot
 mapa_vot_gov %>%
   ggplot() +
-  geom_sf( aes( fill= mais_votado) ,size= .15,
-           color= "grey25", show.legend = TRUE) +
+  geom_sf( aes( fill= mais_votado) ,
+           size= .15, color= "gray20", show.legend = TRUE) +
   theme_void() +
   scale_fill_manual( name= "Candidato",
                      #label= c("Menos de 20%", "20% a 40%"),
                      values= c("red4", "orange4")) +
-  labs(title = "Eleições 2022: Governadores mais Votados por Município em MG", 
+  labs(title = "EleiÃ§Ãµes 2022: Governadores mais Votados por MunicÃ­pio em MG", 
        caption = "DEU CERTO FILHA DA PUTA")
 
 
+# Juntando os dois Mapas
+# NÃ£o repara que o codigo Ã© na correria
+
+mg_result_exec_mais_vot<- full_join( mg_result_pres_wide %>%
+                                           select(mais_votado, lula, jair_bolsonaro,
+                                                  id_municipio),
+                                         mg_result_gov_wide %>%
+                                           select(mais_votado, zema, kalil,
+                                                  id_municipio)
+                                         ,by =  "id_municipio",
+                                         suffix = c("_pres","_gov")) %>%
+      mutate(dois_mais = case_when(
+        mais_votado_pres == "Lula" & mais_votado_gov == "Zema" ~ "Lula e Zema",
+        mais_votado_pres == "Lula" & mais_votado_gov == "Kalil" ~ "Lula e Kalil",
+        mais_votado_pres == "Bolsonaro" & mais_votado_gov == "Zema" ~ "Bolsonaro e Zema",
+        mais_votado_pres == "Bolsonaro" & mais_votado_gov == "Kalil" ~ "Bolsonaro e Kalil",
+        TRUE ~ "Outros"
+      )) %>%
+  relocate(id_municipio,
+           dois_mais)
+
+mapa_vot_gov <-  full_join( x = mapa_munic_mg %>%
+                              mutate(id_municipio= as.character(code_muni)),
+                            y = mg_result_exec_mais_vot
+                            ,by =  "id_municipio")
+
+mapa_vot_gov %>%
+  ggplot() +
+  geom_sf( aes( fill= dois_mais) ,
+           size= .15, color= "gray20", show.legend = TRUE) +
+  theme_void() +
+  scale_fill_manual( name= "Candidato",
+                     #label= c("Menos de 20%", "20% a 40%"),
+                     values= c("dodgerblue3","red2", "brown1")
+                     ) +
+  labs(title = "EleiÃ§Ãµes 2022: Pres. e Gov. mais Votados",
+        subtitle = "Votos por MunicÃ­pio em MG")
+
+
+
+as_tibble(mapa_vot_gov)%>%
+  count(dois_mais, sort = TRUE)
